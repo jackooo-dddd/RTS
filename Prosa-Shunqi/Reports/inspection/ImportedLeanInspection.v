d539457965343e6f5c980f9142621d04e69c8491
Shunqi Wang <shunqi.wang@mail.utoronto.ca>
2026-09-24 23:06:55 +0800

    377 Declartion Finished

--- Prosa-Shunqi/Reports/inspection/ImportedLeanInspection.v
+++
@@ -1,8 +0,0 @@
-From LeanImport Require Import Lean.
-From FoundationImported Require Import ImportedTime.
-
-Set Printing All.
-Print Nat.
-Print Nat_zero.
-Print Nat_succ.
-Print Prosa_Behavior_Time_instant.
